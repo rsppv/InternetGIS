f644c7c8a6cb2759dc72b79f9f360b701505041e
sergeykonkin <sergey@konk.in>
2015-03-03 14:20:16 +0600

    city search

--- js/controllers.js
+++ js/controllers.js
@@ -6,26 +6,72 @@ var InternetGis = angular.module('InternetGis', []);
 
 InternetGis.controller('MainCtrl', function ($scope) {
 
-	angular.element(document).ready(function(){
-		initialize();
-	    setMapWidth();
-	    $(window).on('resize orientationChanged', setMapWidth);		
-	});
+  var geocoder;
+  var map;
 
-	$scope.cities = [    
-		{'name': 'Томск' },
-	    {'name': 'Новосибирск' },
-	    {'name': 'Москва' }
-  	];
 
-  	$scope.addCity = function(){
-  		if (!$scope.cityToAdd) { return };
-  		var newCity = {'name': $scope.cityToAdd};
-  		$scope.cities.push(newCity);
-  	};
+  function initialize() {
+      geocoder = new google.maps.Geocoder();
+      var mapOptions = {
+          center: new google.maps.LatLng(56.497303, 84.972582),
+          zoom: 9,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      map = new google.maps.Map(document.getElementById("map_canvas"),
+          mapOptions);
+  }
 
-  	$scope.removeCity = function(){
-  		
-  	};
+  function setMapWidth() {
+    var cpWidth = $("#control_panel").width();
+    var mapWidth = $("body").innerWidth() - cpWidth - 40;
+    $("#map_canvas").width(mapWidth);
+  }
+
+
+
+
+  /* Initialization */
+  angular.element(document).ready(function(){
+    initialize();
+    setMapWidth();
+    $(window).on('resize orientationChanged', setMapWidth); 
+  });
+
+
+  /* Properties */
+  $scope.cities = [];
+
+
+  /* Methods */
+
+  $scope.addCity = function(){
+    if (!$scope.newCityName) { return };
+    
+    geocoder.geocode( { 'address': $scope.newCityName}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+
+
+        var name = results[0].formatted_address;
+        var location = results[0].geometry;
+
+        var newCity = {'name': name, "location": location};
+        $scope.cities.push(newCity);
+        $scope.newCityName = null; 
+        $scope.$apply();
+      } else {        
+        $("#city-address").notify("Ошибка: " + status, { position: "bottom"});
+      }
+    });
+  };
+
+  $scope.removeCity = function(index){
+    $scope.cities.splice(index, 1);
+  };
 
 });
--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
+var geocoder;
+var map;
+var geocodeResult;
+
 function initialize() {
+    geocoder = new google.maps.Geocoder();
     var mapOptions = {
         center: new google.maps.LatLng(56.497303, 84.972582),
         zoom: 9,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById("map_canvas"),
+    map = new google.maps.Map(document.getElementById("map_canvas"),
         mapOptions);
 }
 
 function setMapWidth() {
-	var cpWidth = $("#control_panel").width();
-	var mapWidth = $("body").innerWidth() - cpWidth - 40;
-	$("#map_canvas").width(mapWidth);
+  var cpWidth = $("#control_panel").width();
+  var mapWidth = $("body").innerWidth() - cpWidth - 40;
+  $("#map_canvas").width(mapWidth);
+}
+
+function codeAddress() {
+  var address = document.getElementById('city-address').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      geocodeResult = status;
+    }
+  });
 }

f55b5e5c294f13d2e13b69cf2585d13ce43d064f
Sergey Konkin <sergey@konk.in>
2015-03-03 23:05:17 +0600

    search improvments

--- js/controllers.js
+++ js/controllers.js
@@ -9,7 +9,6 @@ InternetGis.controller('MainCtrl', function ($scope) {
   var geocoder;
   var map;
 
-
   function initialize() {
       geocoder = new google.maps.Geocoder();
       var mapOptions = {
@@ -18,7 +17,13 @@ InternetGis.controller('MainCtrl', function ($scope) {
           mapTypeId: google.maps.MapTypeId.ROADMAP
       };
       map = new google.maps.Map(document.getElementById("map_canvas"),
-          mapOptions);
+          mapOptions); 
+
+      var input = document.getElementById('city-search');
+      var options = {
+        types: ['(cities)'],
+        componentRestrictions: {country: 'ru'} };
+      var autocomplete = new google.maps.places.Autocomplete(input, options);
   }
 
   function setMapWidth() {
@@ -26,9 +31,6 @@ InternetGis.controller('MainCtrl', function ($scope) {
     var mapWidth = $("body").innerWidth() - cpWidth - 40;
     $("#map_canvas").width(mapWidth);
   }
-
-
-
 
   /* Initialization */
   angular.element(document).ready(function(){
@@ -65,7 +67,7 @@ InternetGis.controller('MainCtrl', function ($scope) {
         $scope.newCityName = null; 
         $scope.$apply();
       } else {        
-        $("#city-address").notify("Ошибка: " + status, { position: "bottom"});
+        $("#city-search").notify("Ошибка: " + status, { position: "bottom"});
       }
     });
   };
